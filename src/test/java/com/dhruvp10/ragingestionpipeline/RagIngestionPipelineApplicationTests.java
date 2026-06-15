3405376c926dae6b3750160e25984e75c1221ce5
DhruvParmar10 <dhruvparmar3840@gmail.com>
2026-06-15 18:22:08 +0530

    setup: docker compose.yaml for kafka, kafka ui, ollama, pgvector.

---
+++ src/test/java/com/dhruvp10/ragingestionpipeline/RagIngestionPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvp10.ragingestionpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagIngestionPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
